2d6dd67b9796076b0ada8752e13aa46cd2fb3799
Andrii <andrew.belous1412@gmail.com>
2023-07-13 13:04:40 +0300

    update serve path

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,16 +1,18 @@
-import { Command } from "commander";
-import { serve } from "local-api";
+import { Command } from 'commander';
+import { serve } from 'local-api';
+import path from 'path';
 
 export const serveCommand = new Command()
-	.command("serve [filename]")
-	.description("Open a file for editing")
-	.option("-p, --port <number>", "port to run server on", "4005")
-	.action((filename = "notebook.js", options: { port: string }) => {
+	.command('serve [filename]')
+	.description('Open a file for editing')
+	.option('-p, --port <number>', 'port to run server on', '4005')
+	.action((filename = 'notebook.js', options: { port: string }) => {
 		let { port } = options;
+		const dir = path.join(process.cwd(), path.dirname(filename));
 
-		if (port.charAt(0) === "=") {
+		if (port.charAt(0) === '=') {
 			port = port.substring(1);
 		}
 
-		serve(parseInt(port), filename, "/");
+		serve(parseInt(port), path.basename(filename), dir);
 	});
